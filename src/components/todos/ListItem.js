9ce71e76cbe74926fb8f19973b2cd083bf6838e7
Paul Mould <paul.mould@eleadcrm.com>
2017-03-14 09:59:29 -0500

    hide checkbox on edit

--- src/components/todos/ListItem.js
+++ src/components/todos/ListItem.js
@@ -26,11 +26,11 @@ class ListItem extends React.Component {
 
         return (
             <div onDoubleClick={(e) => {editTodo(todo)}} className="row row--list-item">
-                <input className="list-item__checkbox" type="checkbox" onDoubleClick={e => e.stopPropagation()} onChange={(e) => actions.updateTodoChecked(e.target.checked, todo.get('id'))} checked={todo.get('completed')}/>
                 {editable ?
-                <input className="edit-item" autoFocus="true" type="text" onBlur={this.onBlur} onKeyDown={this.onKeyDown} onChange={(e) => updateTodo(e.target.value)} value={editText}/>
+                <input className="edit-item edit-item--input" autoFocus="true" type="text" onBlur={this.onBlur} onKeyDown={this.onKeyDown} onChange={(e) => updateTodo(e.target.value)} value={editText}/>
                 :
-                [<span key='edit-item' className="edit-item" style={{textDecoration: (todo.get('completed')) ? 'line-through': 'initial', color: (todo.get('completed')) ? 'gray': 'black'}}>
+                [<input key='check' className="list-item__checkbox" type="checkbox" onDoubleClick={e => e.stopPropagation()} onChange={(e) => actions.updateTodoChecked(e.target.checked, todo.get('id'))} checked={todo.get('completed')}/>,
+                    <span key='edit-item' className="edit-item" style={{textDecoration: (todo.get('completed')) ? 'line-through': 'initial', color: (todo.get('completed')) ? 'gray': 'black'}}>
                     {todo.get('title')}
                 </span>,
                 <span className="cancel-item" key='cancel-item' onClick={e => actions.removeTodo(todo.get('id'))}>X</span>]
